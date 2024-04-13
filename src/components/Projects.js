f964ca4f77ad99fef2487ee3ff1ec69e671cee5d
joshkkuhn@gmail.com <joshkkuhn@gmail.com>
2024-04-13 12:47:54 +0100

    Git fix

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,24 +1,24 @@
-import React, { useState } from "react";
-import "../styles/projects.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch, faFilter, faPlus } from "@fortawesome/free-solid-svg-icons";
+import React, { useState } from 'react';
+import '../styles/projects.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch, faFilter, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 const projectsData = [
   {
-    name: "Gourmet Guide",
-    status: "TASK DUE 19/05"
+    name: 'Gourmet Guide',
+    status: 'IN PROGRESS',
   },
   {
-    name: "Surreal Estate",
-    status: "TASK DUE TODAY"
+    name: 'Surreal Estate',
+    status: 'DUE',
   },
   {
-    name: "Portfolio",
-    status: "LATE"
+    name: 'Portfolio',
+    status: 'LATE',
   },
   {
-    name: "Breakout Game",
-    status: "COMPLETE"
+    name: 'Breakout Game',
+    status: 'COMPLETE',
   },
 ];
 
@@ -28,7 +28,7 @@ const mappedProjectsData = projectsData.map((project, index) => ({
 }));
 
 function Projects() {
-  const [searchTerm, setSearchTerm] = useState("");
+  const [searchTerm, setSearchTerm] = useState('');
 
   const filteredProjects = mappedProjectsData
     .filter(({ name }) => {
@@ -38,13 +38,13 @@ function Projects() {
     .sort((a, b) => a.name.localeCompare(b.name));
 
   return (
-    <div className="projects-container">
-      <div className="projects-container-search">
-        <div className="projects-container-search-input">
-          <FontAwesomeIcon icon={faSearch} className="search-icon" />
+    <div className='projects-container'>
+      <div className='projects-container-search'>
+        <div className='projects-container-search-input'>
+          <FontAwesomeIcon icon={faSearch} className='search-icon' />
           <input
-            type="text"
-            placeholder="Search by name"
+            type='text'
+            placeholder='Search by name'
             value={searchTerm}
             onChange={(e) => setSearchTerm(e.target.value)}
           />
@@ -52,10 +52,10 @@ function Projects() {
         <FontAwesomeIcon icon={faFilter} />
         <FontAwesomeIcon icon={faPlus} />
       </div>
-      <div className="projects-container-cards-group">
+      <div className='projects-container-cards-group'>
         {filteredProjects.map((project) => (
-          <div key={project.id} className="projects-container-card">
-              {project.name} {project.status} <button>Show</button>
+          <div key={project.id} className='projects-container-card'>
+            {project.name} {project.status} <button>Show</button>
           </div>
         ))}
       </div>
